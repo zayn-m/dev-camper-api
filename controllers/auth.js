3f048ea09596fae592c1ece5bb00c051799df213
Zayn484 <zain.hassan1212@gmail.com>
2020-03-31 13:24:37 +0500

    added admin routes

--- controllers/auth.js
+++ controllers/auth.js
@@ -55,6 +55,70 @@ exports.login = asyncHandler(async (req, res, next) => {
     sendTokenResponse(user, 200, res);
 });
 
+// @desc      Log user out / clear cookie
+// @route     GET /api/v1/auth/logout
+// @access    Private
+exports.logout = asyncHandler(async (req, res, next) => {
+    res.cookie("token", "none", {
+        expires: new Date(Date.now() + 10 * 1000),
+        httpOnly: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: {},
+    });
+});
+
+// @desc      Get current logged in user
+// @route     POST /api/v1/auth/me
+// @access    Private
+exports.getMe = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user._id);
+
+    res.status(200).json({
+        success: true,
+        data: user,
+    });
+});
+
+// @desc      Update user details
+// @route     PUT /api/v1/auth/updatedetails
+// @access    Private
+exports.updateDetails = asyncHandler(async (req, res, next) => {
+    const fieldsToUpdate = {
+        name: req.body.name,
+        email: req.body.email,
+    };
+
+    const user = await User.findByIdAndUpdate(req.user._id, fieldsToUpdate, {
+        new: true,
+        runValidators: true,
+    });
+
+    res.status(200).json({
+        success: true,
+        data: user,
+    });
+});
+
+// @desc      Update password
+// @route     PUT /api/v1/auth/updatepassword
+// @access    Private
+exports.updatePassword = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.user._id).select("+password");
+
+    // Check current password
+    if (!(await user.matchPassword(req.body.currentPassword))) {
+        return next(new ErrorResponse("Password is incorrect", 401));
+    }
+
+    user.password = req.body.newPassword;
+    await user.save();
+
+    sendTokenResponse(user, 200, res);
+});
+
 // @desc    Forget password
 // @route   POST /api/v1/auth/forgotpassword
 // @access  Public
--- routes/auth.js
+++ routes/auth.js
@@ -2,10 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 const authControllers = require("../controllers/auth");
+const { protect } = require("../middleware/auth");
 
 router.route("/register").post(authControllers.register);
 router.route("/login").post(authControllers.login);
+router.route("/logout").get(authControllers.logout);
+router.route("/me").get(protect, authControllers.getMe);
 router.route("/forgotpassword").post(authControllers.forgotPassword);
 router.route("/resetpassword/:resetToken").put(authControllers.resetPassword);
+router.route("/updatedetails").put(protect, authControllers.updateDetails);
+router.route("/updatepassword").put(protect, authControllers.updatePassword);
 
 module.exports = router;
